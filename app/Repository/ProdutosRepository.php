b11a4a95ce410eb9b293ff063332a0793dd2fc0b
jvtuta <joaovoliveira2399@gmail.com>
2022-01-05 14:42:26 -0300

    Repositorio

--- app/Repository/ProdutosRepository.php
+++ app/Repository/ProdutosRepository.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Repository;
+use Illuminate\Support\Facades\DB;
+
 
 class ProdutosRepository extends Repository {
     private $produtos;
@@ -15,72 +17,96 @@ class ProdutosRepository extends Repository {
         $this->filiais = $filiais;
         $this->filialEstoque = $filialEstoque;
         $this->ultimoDiaMes = date('t', mktime(0, 0, 0, $mes, '01', $ano));
-        $this->mesFim = $ultimoDiaMes . '.' . $mes . '.' . $ano;
+        $this->mesFim = $this->ultimoDiaMes . '.' . $mes . '.' . $ano;
         $this->mesInicio = '01.' . $mes . '.' . $ano;
-
+        
         $this->produtos();
     }
 
-    public function produtos()
+    private function produtos()
     {
+        
         $this->produtos = DB::table('FC03000')
             ->select()
             ->whereRaw("GRUPO NOT IN ('D', 'O') AND INDDEL = 'N'")
-            ->count();
-        return;     
+            ->get()
+            ->toArray();
     }
 
     /**
      * @return Produtos    
     */
 
-    public function getProdutos()
+    public function get()
     {
-        return $this->produtos;
+        $resultArray = Array();
+        foreach($this->produtos as $produto) {
+            $cma = @($produto->PRCOMN / $produto->PRVEN) * 100;
+            $cma = number_format(($cma), 2, ',', '.');
+            $mkp = @($produto->PRVEN / $produto->PRCOM);
+            $mkp = number_format(($mkp), 2, ',', '.');
+            $consumo = $this->consumo($produto);
+            $res = [
+                'Cod. Produto'=> $produto->CDPRO,
+                'Produto' => $produto->DESCRPRD,
+                'Curva'=> $produto->CURVA,
+                'Grupo' => $produto->GRUPO,
+                'Estoque Atual' => $this->estoque($produto),
+                'Consumo' => $consumo,
+                'Frequencia' => $this->frequencia($produto, $consumo),
+                'R$ Compra' => number_format(($produto->PRCOMN), 2, ',', '.'),
+                'R$ Venda' => number_format(($produto->PRVEN), 2, ',', '.'),
+                'CMA %' => $cma,
+                'MKP' => $mkp
+            ];
+            array_push($resultArray, $res);
+        }
+        return $resultArray;
     }
 
     /**
      * @param CDPRO_PRODUTO_
      * @return ESTOQUE_PRODUTO_
      */
-    public function estoque($produto) 
+    private function estoque($produto) 
     {
         $estoque = DB::table('FC03140')
-            ->selectRaw('SUM(ESTAT - SAIDATR)')
-            ->where('CDPRO', "'$produto->CDPRO'")
-            ->whereRaw("CDFIL IN '$this->filiais'")
-            ->get();
-        return $estoque->SUM;                                                                            
+            ->select()
+            ->where('CDPRO', $produto->CDPRO)
+            ->whereRaw("CDFIL IN $this->filiais")
+            ->get()->sum('ESTAT - SAIDATR');
+        return $estoque;                                                                            
     }
     /**
      * @param CDPRO_DO_PRODUTO_
      */
-    public function consumo($produto)
+    private function consumo($produto)
     {
         $consumo = DB::table('FC03110')
-            ->selectRaw('SUM(ACSAIDAQT)')
-            ->where('CDPRO', "'$produto->CDPRO'")
+            ->select()
+            ->where('CDPRO', $produto->CDPRO)
             ->whereRaw("CDFIL in $this->filiais")
-            ->where('ANORF', "'$this->ano'")
-            ->where('MESRF', "'$this->mes'")
-            ->get();
-        return $consumo->SUM;                        
+            ->where('ANORF', $this->ano)
+            ->where('MESRF', $this->mes)
+            ->get()->sum('ACSAIDAQT');
+        
+        return $consumo;                        
     }
     /**
      * @param CDPRO_PRODUTO_
      * @param CONSUMO_PRODUTO_
      */
-    public function frequencia($produto, $consumo)
+    private function frequencia($produto, $consumo)
     {
-        if($consumo->SUM == '0') {
-            return;
+        if($consumo == '0') {
+            return 0;
         }
         $frequencia = DB::table('FC12110')
-            ->selectRaw("COUNT(DISTINCT(NRRQU))")
-            ->where('CDPRO', "'$produto->CDPRO'")
+            ->select(DB::raw('COUNT(DISTINCT(NRRQU))'))
+            ->where('CDPRO', $produto->CDPRO)
             ->whereRaw("CDFILE in $this->filialEstoque")
-            ->whereRaw("DTENTR BETWEEN '$this->mesInicio' AND '$this->mesFim'")
-            ->get();
-        return $frequencia->COUNT;
+            ->whereRaw("DTENTR BETWEEN cast('$this->mesInicio' as date) and cast('$this->mesFim' as date)")
+            ->get()->toArray();
+        return $frequencia[0]->COUNT;
     }
 }
