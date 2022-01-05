d92d6dbf92cbc356745a70a98736e6a81bd5bccf
jvtuta <joaovoliveira2399@gmail.com>
2022-01-05 18:06:55 -0300

    Header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,9 +6,44 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Painel de Produtos</title>
+    <link rel="shortcut icon" href="https://farmacotecnica.com.br/favicon.ico">
+    <style>
+  
+        * {
+        margin: 0;
+        padding: 0;
+        overflow-x: hidden;
+        font-family: 'swiss_721regular' !important;
+        }
+        div.titulo h1 {
+            font-weight: 900;
+            font-size: 4rem;
+            color: white;
+        }
+        nav.header {
+            background-color: black;
+            margin-bottom: 50px;
+        }
+    </style>
 </head>
 <body>
-
+    <nav class="navbar navbar-expand-lg navbar-light header">
+        <div class="container-fluid">
+          <div>
+            <a class="navbar-brand" href="#">
+              <img src="{{asset('logo-farmacotecnica.png')}}" alt="" width="220">
+            </a>
+          </div>
+          <div class="titulo">
+            <h1>Painel de Produtos</h1>
+          </div>
+          <div>
+            <a href="" class="navbar-brand">
+              <img src="{{asset('logo-mram.png')}}" alt="" width="220">
+            </a>
+          </div>
+        </div>
+      </nav>
     
     <div id="app">
         @yield('content')
