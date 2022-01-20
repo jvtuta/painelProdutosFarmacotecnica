6f81f58b7f72ffb392ae841c7c009e68c4a84e8f
jvtuta <joaovoliveira2399@gmail.com>
2022-01-20 09:18:46 -0300

    Maintence mode

---
+++ resources/views/maintence.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('content')
+
+
+<div class="d-flex align-items-center justify-content-center"> 
+    <div >
+        <p>Novo endereço = 10.10.1.123, qualquer duvida ligar no CPD</p>
+
+    </div>
+</div>
+@endsection
