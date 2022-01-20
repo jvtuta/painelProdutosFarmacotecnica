d6c923ee3f6b717b2178ad7aa6592be3be7320a6
jvtuta <joaovoliveira2399@gmail.com>
2022-01-20 09:21:40 -0300

    Maintence mode

--- resources/views/maintence.blade.php
+++ resources/views/maintence.blade.php
@@ -6,7 +6,7 @@
 <div class="d-flex align-items-center justify-content-center"> 
     <div >
         <p>
-            Novo endereço = <a href="http://10.10.1.123">10.10.1.123</a>, qualquer duvida ligar no CPD</p>
+            Novo endereço = <a href="http://10.10.1.123">10.10.1.123</a>, qualquer dúvida ligar no CPD</p>
 
     </div>
 </div>
